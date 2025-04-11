e7b3908e575c6ca76cb839b4472c9caf00d5078f
Qualia-Li <Qualia-Li@users.noreply.github.com>
2025-04-10 17:49:03 -0700

    feat: Add Google tag to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-2X617E3BDW"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-2X617E3BDW');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
